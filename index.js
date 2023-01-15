75a98fcd7edc3f512c174e3bdaf9c7e2c00dbda6
tadilbek11kz <tadilbek11kz@gmail.com>
2023-01-16 03:17:00 +0600

    feat: install package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const app = express()
 
 app.use(bodyParser.json())
