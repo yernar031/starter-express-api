aef57db52378e19d41c44ae045bfe4c78e78c8f9
tadilbek11kz <tadilbek11kz@gmail.com>
2023-01-16 02:46:26 +0600

    feat: add 7 answers

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.json())
+
 app.post('/parse', (req, res) => {
-    console.log("Just got a request!")
-    console.log(req.body)
+    lead_data = req.body.lead_data;
+    fullName = lead_data.answer_1;
+    number = lead_data.answer_2;
+    region = lead_data.answer_3
+    filter = lead_data.answer_4;
+    limescale = lead_data.answer_5;
+    time = lead_data.answer_6
+    price = lead_data.answer_7;
+    console.log(fullName, number, region, filter, limescale, time, price)
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
